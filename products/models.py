e5507ee720a8047c09d1a67fc9fb1053cc8d803d
oldpilluwu <fawwazamin29@gmail.com>
2020-12-24 23:13:30 +0600

    website done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Product(models.Model):
+    title = CharField(max_length=255)
+    pub_date = models.DateField()
+    body = models.TextField()
+    url = models.TextField(default='www.google.com')
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
+    
+    
--- products/views.py
+++ products/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects.all()
+    return render(request, 'products/home.html', {'products': products})
+
+
+@login_required(login_url='/accounts/signup')
+def create(request):
+    if request.method == 'POST':
+        print(request.POST)
+        if request.POST.get('title') and request.POST.get('body') and request.POST.get('url') and request.FILES.get('icon') and request.FILES.get('image'):
+            product = Product()
+            product.title = request.POST.get('title')
+            product.body = request.POST.get('body')
+            if request.POST.get('url').startswith('http://') or request.POST.get('url').startswith('https://'):
+                product.url = request.POST.get('url')
+            else:
+                product.url = 'http://' + request.POST.get('url')
+            product.icon = request.FILES.get('icon')
+            product.image = request.FILES.get('image')
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/' + str(product.id))
+        else:
+            return render(request, 'products/create.html', {'error': 'All field are required'})
+    else:
+        return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+
+@login_required(login_url='/accounts/signup')
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return render(request, 'products/detail.html', {'product': product})
     
